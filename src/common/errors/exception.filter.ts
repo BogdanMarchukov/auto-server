07f839baa92b3fccae3a8581b42da083c8a30115
Bogdan Marchulov <bogdan_info@mail.ru>
2024-08-24 15:58:19 +0300

    fix: exaption filter

--- src/common/errors/exception.filter.ts
+++ src/common/errors/exception.filter.ts
@@ -1,9 +1,18 @@
+import { ValidationError } from "class-validator";
 import { Context, Next } from "koa";
 
 export async function exceptionFilter(ctx: Context, next: Next) {
   try {
     await next();
   } catch (err) {
+    console.error(err);
+    if (Array.isArray(err) && err[0] instanceof ValidationError) {
+      ctx.status = 400;
+      ctx.body = {
+        message: JSON.stringify(err[0].constraints || {}),
+      };
+      return;
+    }
     ctx.status = err.status || 500;
     ctx.body = {
       message: err.message || "Internal Server Error",
